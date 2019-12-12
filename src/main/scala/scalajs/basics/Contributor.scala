3ac947e80e28e46f0851636a3c15ff65a24b8485
Jafar Sadik <dzafar.sadik@gmail.com>
2019-12-12 17:38:10 +0000

    Simplified code by removing unnecessary `js.native` declarations

--- src/main/scala/scalajs/basics/Contributor.scala
+++ src/main/scala/scalajs/basics/Contributor.scala
@@ -4,8 +4,8 @@ import scala.scalajs.js
 
 @js.native
 trait Contributor extends js.Object {
-  val login: String = js.native
-  val html_url: String = js.native
-  val avatar_url: String = js.native
-  val contributions: Int = js.native
+  val login: String
+  val html_url: String
+  val avatar_url: String
+  val contributions: Int
 }
